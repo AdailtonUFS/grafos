af5ed6d55cdb3453ad2242e3f692a668ffa64155
Adailton Moura <contatoadailtonmoura@gmail.com>
2023-06-19 01:18:05 -0300

    create insert logic in Matriz

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from src.v2.Grafo import Grafo
+from src.v2.Vertice import Vertice
 
 print(type(Grafo("Matriz")))
 print(type(Grafo("Estrutura de Adjacência")))
 print(type(Grafo("Outro")))
+print('\n')
+
+grafomatriz = Grafo("Matriz")
+grafomatriz.adicionar(Vertice("vertice 1", 0))
--- src/v2/Matriz.py
+++ src/v2/Matriz.py
@@ -1,2 +1,16 @@
+from src.v2.Vertice import Vertice
+
+
 class Matriz:
-    pass
+    matriz:list = []
+    indices:set = set()
+    def adicionar(self, vertice:Vertice, force=False) -> bool:
+        if vertice.indice in self.indices and force == False:
+            print("O item já está na lista.")
+            return False
+
+        self.indices.add(vertice.indice)
+
+        print("inserido")
+        print(self.indices)
+        return True
