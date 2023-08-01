e51a209f2025298c0f01a57eb1094243a0a7a5d8
Adailton <contatoadailtonmoura@gmail.com>
2023-08-01 18:48:58 -0300

    adding button tag to resolve error

--- graphs/ui/Button/VertexButton.py
+++ graphs/ui/Button/VertexButton.py
@@ -28,13 +28,15 @@ class VertexButton:
         if self.text is not None:
             self.canvas.delete(self.text)
 
+        button_tag = 'vertex_button'
         self.object = VertexFigure(self.canvas, [x1, y1, x2, y2]).make()
+        self.canvas.addtag_withtag(button_tag, self.object)  # Adicionar a nova tag ao botão
 
         text_x = (x1 + x2) / 2
         text_y = (y1 + y2) / 2
-        self.text = self.canvas.create_text(text_x, text_y, text="v1", fill="black")
+        self.text = self.canvas.create_text(text_x, text_y, text="v1", fill="black", tags=button_tag)
 
-        self.canvas.tag_bind(self.object, '<Button-1>', self.on_click)
+        self.canvas.tag_bind(button_tag, '<Button-1>', self.on_click)
 
     def on_canvas_resize(self, event):
         self.update_button_position()
