8f077fdb22ab35d7100e1385ce1ce2810597fd89
Adailton <contatoadailtonmoura@gmail.com>
2023-08-01 18:28:52 -0300

    add grid system to option menubar

--- graphs/ui/OptionsMenu.py
+++ graphs/ui/OptionsMenu.py
@@ -5,8 +5,11 @@ from graphs.ui.Button.VertexButton import VertexButton
 
 class OptionsMenu(tk.Canvas):
     def __init__(self, master=None):
-        super().__init__(master)
+        super().__init__(master, width=10)
         self.config(bg="white")
         self.grid(row=0, column=1, sticky="nsew", padx=(0, 10), pady=10)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
         self.vertex_button = VertexButton(self)
-        # self.edge_button = EdgeButton(master=self)
+        self.edge_button = EdgeButton(master=self)
