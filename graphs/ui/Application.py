69c19b76e55f5c0acacdf4b0450ecd7d60b30e91
Adailton Moura <contatoadailtonmoura@gmail.com>
2023-08-04 00:39:06 -0300

    remove scrollbar

--- graphs/ui/Application.py
+++ graphs/ui/Application.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from OptionsMenu import OptionsMenu
 import platform
+
+from OptionsMenu import OptionsMenu
 
 from graphs.ui.DrawableArea import DrawableArea
 
@@ -8,19 +9,27 @@ from graphs.ui.DrawableArea import DrawableArea
 class Application(tk.Tk):
     def __init__(self, master=None):
         super().__init__()
-
         self.config(bg="white")
-        self.columnconfigure(0, weight=95)  # 95% da largura para a coluna 0
-        self.columnconfigure(1, weight=5)  # 5% da largura para a coluna 1
-        self.rowconfigure(0, weight=1)  # 100% da altura para a linha 0
+        self.layout()
+        self.geometry("%dx%d" % (800, 600))
+        self.maximized()
 
+        self.drawable_area = DrawableArea(master=self)
+        self.drawable_area.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+
+        self.option_menu = OptionsMenu(master=self)
+
+    def layout(self):
+        self.columnconfigure(0, weight=95)
+        self.columnconfigure(1, weight=5)
+        self.rowconfigure(0, weight=1)
+
+
+    def maximized(self):
         if platform.system() == "Linux":
             self.attributes('-zoomed', True)
         else:
             self.state('zoomed')
-
-        self.drawable_area = DrawableArea(master=self)
-        self.option_menu = OptionsMenu(master=self)
 
 
 app = Application()
--- graphs/ui/DrawableArea.py
+++ graphs/ui/DrawableArea.py
@@ -8,7 +8,6 @@ class DrawableArea(tk.Canvas):
     def __init__(self, master=None):
         super().__init__(master)
         self.config(bg="white")
-        self.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
 
     def create_vertex(self):
         Vertex(self)
